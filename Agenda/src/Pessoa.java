96ed330c559311444fca426250973c4402dee782
William <meykos@gmail.com>
2011-09-10 09:47:20 -0300

    Criada a classe Pessoa.

---
+++ Agenda/src/Pessoa.java
@@ -0,0 +1,21 @@
+public class Pessoa {
+	private String nome;
+	private String telefone;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
